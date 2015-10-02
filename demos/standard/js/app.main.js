64b30ecf41b12f921854a65d16ea7ef204287c52
Flavien Grevillot <flavien@quatrecentquatre.com>
2015-10-02 15:20:44 -0400

    Demos refactored

---
+++ demos/standard/js/app.main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var $scope = $('#infinite');
+    var infinite = new Me.infinite({
+        debug: true,
+        infinite_container: $scope,
+        infinite_loader: $scope.find('.loader'),
+        item_per_page: 4,
+        page_total: 10,
+        toggler_button: $scope.find('.toggler'),
+        event_onload: function(scope) {
+            for (var i = 0; i < scope.options.item_per_page; i++) {
+                scope.$el.find('.item-list').append($('#item-t').html());
+            }
+            Me.dispatch.emit('InfiniteMe.onload');
+        }
+    });
+});
---
+++ demos/toggler/js/app.main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var $scope = $('#infinite');
+    var infinite = new Me.infinite({
+        debug: true,
+        infinite_container: $scope,
+        infinite_offset: -5,
+        infinite_loader: $scope.find('.loader'),
+        item_per_page: 4,
+        page_total: 10,
+        toggler_button: $scope.find('.toggler'),
+        toggler_page_offset: -1,
+        event_onload: function(scope) {
+            for (var i = 0; i < scope.options.item_per_page; i++) {
+                scope.$el.find('.item-list').append($('#item-t').html());
+            }
+            Me.dispatch.emit('InfiniteMe.onload');
+        }
+    });
+});
